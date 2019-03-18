cf58991d425ca7fe0f36ff860c2fb7b6cd79970f
Vince Carollo <vincecarollo@gmail.com>
2019-03-18 12:47:27 -0600

    Move user prompt to game class

--- lib/flashcard_runner.rb
+++ lib/flashcard_runner.rb
@@ -6,24 +6,11 @@ require './lib/loader'
 require './lib/game'
 require 'pry'
 
-p "What kind of input would you like to use?"
-
-input = ''
-#Keeps getting input until it is valid
-until input == 'txt' || input == 'yml' || input == 'api'
-  p "Type One of the following"
-  p "1) API"
-  p "2) YML"
-  p "3) TXT"
-  input = gets.chomp.downcase
-end
-
+game = Game.new
+input = game.input
 reader = Loader.new(input).reader
 cards = reader.cards
 deck = Deck.new(cards)
 round = Round.new(deck)
 categories = reader.category_arr.uniq
-
-
-game = Game.new
 game.start(categories, round)
