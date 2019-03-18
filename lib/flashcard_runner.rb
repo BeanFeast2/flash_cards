e8cca8d7830cc3adb9f65ec62a8ab28fb820c14f
Vince Carollo <vincecarollo@gmail.com>
2019-03-17 21:42:41 -0600

    Remove global variables and removed redundancies

--- lib/flashcard_runner.rb
+++ lib/flashcard_runner.rb
@@ -6,12 +6,8 @@ require './lib/loader'
 require 'pry'
 
 p "What kind of input would you like to use?"
-p "Type One of the following"
-p "1) API"
-p "2) YML"
-p "3) TXT"
 
-input = gets.chomp.downcase
+input = ''
 #Keeps getting input until it is valid
 until input == 'txt' || input == 'yml' || input == 'api'
   p "Type One of the following"
@@ -24,31 +20,31 @@ end
 reader = Loader.new(input).reader
 cards = reader.cards
 deck = Deck.new(cards)
-$round = Round.new(deck)
-$categories = reader.category_arr.uniq
+round = Round.new(deck)
+categories = reader.category_arr.uniq
 
 
-def start
+def start(categories, round)
   #welcome message
-  p "Welcome! You're playing with #{$round.deck.cards.length} cards."
+  p "Welcome! You're playing with #{round.deck.cards.length} cards."
   p '*' * 64
   #game loop until all cards are iterated through
-  until $round.turn_index == $round.deck.cards.length
-    p "This is card #{$round.turn_index + 1} out of #{$round.deck.cards.length}"
-    p "Question: #{$round.current_card.question}"
+  until round.turn_index == round.deck.cards.length
+    p "This is card #{round.turn_index + 1} out of #{round.deck.cards.length}"
+    p "Question: #{round.current_card.question}"
     #allows input to match answer despite case
     input = gets.chomp.downcase
-    $round.take_turn(input)
-    p $round.turns[$round.turn_index - 1].feedback
-    p '*' * 64 if $round.turn_index != $round.deck.cards.length
+    round.take_turn(input)
+    p round.turns[round.turn_index - 1].feedback
+    p '*' * 64 if round.turn_index != round.deck.cards.length
   end
   #end of game message with stats
   p '*' * 27 + "Game Over!" + '*' * 27
-  p "You had #{$round.number_correct} correct guesses out of #{$round.deck.cards.length} for a total score of #{$round.percent_correct.round(2)}%"
+  p "You had #{round.number_correct} correct guesses out of #{round.deck.cards.length} for a total score of #{round.percent_correct.round(2)}%"
   #prints each category and percentage of correct answers per category
-  $categories.each do |category|
-    p "#{category} - #{$round.percent_correct_by_category(category.delete(' ').to_sym)}%"
+  categories.each do |category|
+    p "#{category} - #{round.percent_correct_by_category(category.delete(' ').to_sym)}%"
   end
 end
 
-start
+start(categories, round)
